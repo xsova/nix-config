28d274a0a5eb5a097ccc696728a62a1ce57fe1e6
Bryce <bryce@bryces.email>
2024-10-16 23:33:04 -0500

    cleanup

--- flake.nix
+++ flake.nix
@@ -2,33 +2,22 @@
   description = "Nix flake for NixOS on a laptop, desktop, server, and Macbook (NixDarwin)";
 
   inputs = {
-    # Specify the nixpkgs repository to use.
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    
-    # Include Home Manager and make it follow (mirro) the version nixpkgs uses.
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # Include Nix Darwin and make it follow nixpkgs.
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
-    # Tool to quickly locate the package providing a certain file in nixpkgs.
     nix-index = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows  = "nixpkgs";
     };
-
-    # Source urls for things not in nixpkgs / that I want a different or more up-to-date version of.
     pinix.url = "github:remi-dupre/pinix"; # Tool to see nix rebuild progress similar to pacman in Arch linux.
     wezterm.url = "github:wez/wezterm?dir=nix"; # Tmuxer and terminal emulator.
     rust-overlay.url = "github:oxalica/rust-overlay";
-
-    # Firmware/tools that are device specific for nixOS.
     hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
@@ -40,9 +29,8 @@
         modules = [
           { system.configurationRevision = self.rev or self.dirtyRev or null; }
           nix-index.darwinModules.nix-index
-          ./hosts/macbook/modules/default.nix
           home-manager.darwinModules.home-manager
-          ./hosts/macbook/home/default.nix
+          ./hosts/macbook/modules/default.nix
         ];
       };
     };
--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -3,10 +3,6 @@
 {
   imports = [
     ./hardware-configuration # FIXME: Make this the output of nixos-generate-config
-    ../../home/common/default.nix
-    ../../home/framework/default.nix
-    ../../modules/common
-    ../../modules/desktop
-    ../../modules/laptop
+    ./disko/default.nix
   ];
 }
