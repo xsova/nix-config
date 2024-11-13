c692fe63f601470c801a16651781724069be69d3
bryce <bryce@xsova.dev>
2024-11-13 14:02:25 -0600

    Update $CODE to fleet instead of vscode

--- home/environment.nix
+++ home/environment.nix
@@ -3,7 +3,7 @@
 {
     home.sessionVariables = {
       EDITOR = "hx";
-      VISUAL = "code";
+      VISUAL = "fleet";
       MANPAGER = "sh -c 'col -bx | bat -l man -p'";
       HOSTNAME = "(hostname -s)";
       IWD = "($PWD)";
