256c236b9cfc398ebfe679dbcb9263d8f3e9a646
bryce <bryce@xsova.dev>
2024-11-19 09:13:40 -0600

    add overlays for programming languages

--- flake.nix
+++ flake.nix
@@ -3,48 +3,30 @@
 
   inputs = {
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/0.1.5.tar.gz";
-    fh = {
-      url = "https://flakehub.com/f/DeterminateSystems/fh/*.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    fw-fanctrl = {
-      url = "github:TamtamHero/fw-fanctrl/packaging/nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    fh = { url = "https://flakehub.com/f/DeterminateSystems/fh/*.tar.gz"; inputs.nixpkgs.follows = "nixpkgs"; };
+    fw-fanctrl = { url = "github:TamtamHero/fw-fanctrl/packaging/nix"; inputs.nixpkgs.follows = "nixpkgs"; };
     hardware.url = "github:NixOS/nixos-hardware/master";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
+    home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
+    hyprland-plugins = { url = "github:hyprwm/hyprland-plugins"; inputs.hyprland.follows = "hyprland"; };
     hyprland.url = "github:hyprwm/Hyprland";
-    lix = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-index = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    lix = { url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nix-darwin = { url = "github:LnL7/nix-darwin"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nix-index = { url = "github:nix-community/nix-index-database"; inputs.nixpkgs.follows = "nixpkgs"; };
     pinix.url = "github:remi-dupre/pinix";
     ragenix.url = "github:yaxitech/ragenix";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
+    rust-overlay.url = "github:oxalica/rust-overlay";
     zig-overlay.url = "github:mitchellh/zig-overlay";
   };
 
   outputs = inputs @ {self, ...}: {
     schemas = inputs.flake-schemas.schemas;
+    overlays = with inputs; [
+      rust-overlay.overlays.default
+      zig-overlay.overlays.default
+    ];
     darwinConfigurations = {
-      port      = import ./hosts/port      {inherit inputs self;};
+      port = import ./hosts/port { inherit inputs self; };
     };
     nixosConfigurations = {
       # starboard = import ./hosts/starboard {inherit inputs self;};
